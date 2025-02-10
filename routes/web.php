753ac14105362a4545a31c589e27b85a08f58712
Ravidev S <ravidev.it@gmail.com>
2025-02-10 13:48:39 +0530

    db added

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,11 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/index', function () {
+    return view('index');
+});
+
+
 Route::get('/about', function () {
     return view('about');
 });
